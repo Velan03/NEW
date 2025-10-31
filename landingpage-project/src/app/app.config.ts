fc71c8c69e4572a0ceece7b4960b5d37e1bc8ba0
velan_sivasankaran <111336249+Velan03@users.noreply.github.com>
2025-11-01 00:12:27 +0530

    Update app.config.ts

--- landingpage-project/src/app/app.config.ts
+++ landingpage-project/src/app/app.config.ts
@@ -3,22 +3,13 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideClientHydration(withEventReplay()),
-
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-      },
-    }),
+ 
   ],
 };
